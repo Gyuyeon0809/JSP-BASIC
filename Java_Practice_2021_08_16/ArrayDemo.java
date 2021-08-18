b623aa08a49ab017b13244a0cabc03dd6779f614
Gyuyeon0809 <88140403+Gyuyeon0809@users.noreply.github.com>
2021-08-18 19:51:53 +0900

    Add files via upload

---
+++ Java_Practice_2021_08_16/ArrayDemo.java
@@ -0,0 +1,9 @@
+public class ArrayDemo {
+   
+    public static void main(String[]args) {
+        int[] array1 = new int[100];
+        for(int i = 0; i<array1.length; i++){
+            array1[i] = i + 1;
+        }
+    }   
+}
